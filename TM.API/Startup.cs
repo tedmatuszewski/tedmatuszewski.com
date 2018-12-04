6346eeba1c750c3d006b1bd8f0cc1a1684c96375
Ted Matuszewski <ted.matuszewski@outlook.com>
2018-12-03 20:24:30 -0500

    Added new solution structure.

--- TM.API/Startup.cs
+++ TM.API/Startup.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace TM.Web
+namespace TM.API
 {
     public class Startup
     {
@@ -23,7 +24,7 @@ namespace TM.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,8 +36,6 @@ namespace TM.Web
             }
 
             app.UseMvc();
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
         }
     }
 }
