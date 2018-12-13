4b3ecb45e627a281bc0888cc79657d039e097ef4
Ted Matuszewski <ted.matuszewski@outlook.com>
2018-12-13 17:53:02 -0500

    Added xamarin projects

---
+++ TM.XAM/TM.XAM/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TM.XAM.Models;
+
+namespace TM.XAM.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
