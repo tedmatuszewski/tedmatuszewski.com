bc562639e85bbfb3c795b92edafa59ea6707f65d
Ted Matuszewski <ted.matuszewski@outlook.com>
2018-12-19 23:43:10 -0500

    Updated icons and app name.

--- TM.XAM/TM.XAM.Android/MainActivity.cs
+++ TM.XAM/TM.XAM.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace TM.XAM.Droid
 {
-    [Activity(Label = "TM.XAM", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Ted M", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
